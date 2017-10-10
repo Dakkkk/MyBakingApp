f3c61b11bd4d7bfe6060c6bc477186ae54cce48e
DaveKun <dawidkunicki88@gmail.com>
2017-10-10 16:37:44 +0200

    Initialized base recipes list fragment

--- app/src/main/java/com/mobileallin/mybakingapp/ui/fragment/RecipesListFragment.java
+++ app/src/main/java/com/mobileallin/mybakingapp/ui/fragment/RecipesListFragment.java
@@ -1,10 +1,35 @@
 package com.mobileallin.mybakingapp.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.mobileallin.mybakingapp.R;
 
 /**
  * Created by Dawid on 2017-10-10.
  */
 
 public class RecipesListFragment extends MvpAppCompatFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipes_list, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
 }
